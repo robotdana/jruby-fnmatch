a64462858d546abf5700d6f254d14cbff89f6b4c
Dana Sherson <robot@dana.sh>
2020-04-26 10:59:09 +1000

    Initial Commit

---
+++ spec/fnmatch_spec.rb
@@ -0,0 +1,93 @@
+RSpec.describe 'fnmatch' do
+  it 'works with literal values' do
+    expect(::File.fnmatch?('aaa', 'aaa')).to be true
+    expect(::File.fnmatch?('aaa', 'a/a')).to be false
+    expect(::File.fnmatch?('aaa', 'aba')).to be false
+  end
+
+  it 'works with single value square brackets' do
+    expect(::File.fnmatch?('a[a]a', 'aaa')).to be true
+    expect(::File.fnmatch?('a[a]a', 'a[a]a')).to be false
+    expect(::File.fnmatch?('a[a]a', 'aba')).to be false
+  end
+
+  it 'works with multiple value square brackets' do
+    expect(::File.fnmatch?('a[ab]a', 'aaa')).to be true
+    expect(::File.fnmatch?('a[ab]a', 'aba')).to be true
+    expect(::File.fnmatch?('a[ab]a', 'a[ab]a')).to be false
+  end
+
+  it 'matches / in square brackets when there are other values' do
+    expect(::File.fnmatch?('a[a/]a', 'aaa')).to be true
+    expect(::File.fnmatch?('a[a/]a', 'a/a')).to be true
+    expect(::File.fnmatch?('a[a/]a', 'aba')).to be false
+  end
+
+  it "matches / in square brackets when there are other values and it's the first value" do
+    expect(::File.fnmatch?('a[/a]a', 'aaa')).to be true
+    expect(::File.fnmatch?('a[/a]a', 'a/a')).to be true
+    expect(::File.fnmatch?('a[/a]a', 'aba')).to be false
+  end
+
+  it "matches / in square brackets when it's alone" do
+    expect(::File.fnmatch?('a[/]a', 'a/a')).to be true
+    expect(::File.fnmatch?('a[/]a', 'a//a')).to be false
+  end
+
+  it 'matches / with *' do
+    expect(::File.fnmatch?('a*a', 'a/a')).to be true
+    expect(::File.fnmatch?('a*a', 'a//a')).to be true
+    expect(::File.fnmatch?('a*a', 'aba')).to be true
+    expect(::File.fnmatch?('a*a', 'abba')).to be true
+  end
+
+  it 'matches / with ?' do
+    expect(::File.fnmatch?('a?a', 'a/a')).to be true
+    expect(::File.fnmatch?('a?a', 'a//a')).to be false
+    expect(::File.fnmatch?('a?a', 'aba')).to be true
+    expect(::File.fnmatch?('a?a', 'abba')).to be false
+  end
+
+  context 'with File::FNM_PATHNAME' do
+    it "doesn't match / in square brackets when there are other values" do
+      expect(::File.fnmatch?('a[a/]a', 'aaa', ::File::FNM_PATHNAME)).to be true
+      expect(::File.fnmatch?('a[a/]a', 'a/a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a[a/]a', 'aba', ::File::FNM_PATHNAME)).to be false
+    end
+
+    it "doesn't match / in square brackets when there are other values and it's the first value" do
+      expect(::File.fnmatch?('a[/a]a', 'aaa', ::File::FNM_PATHNAME)).to be true
+      expect(::File.fnmatch?('a[/a]a', 'a/a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a[/a]a', 'aba', ::File::FNM_PATHNAME)).to be false
+    end
+
+    it "doesn't match / in square brackets when it's alone" do
+      expect(::File.fnmatch?('a[/]a', 'a/a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a[/]a', 'a//a', ::File::FNM_PATHNAME)).to be false
+    end
+
+    it "doesn't match / with *" do
+      expect(::File.fnmatch?('a*a', 'a/a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a*a', 'a//a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a*a', 'aba', ::File::FNM_PATHNAME)).to be true
+      expect(::File.fnmatch?('a*a', 'abba', ::File::FNM_PATHNAME)).to be true
+    end
+
+    it "doesn't match / with ?" do
+      expect(::File.fnmatch?('a?a', 'a/a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a?a', 'a//a', ::File::FNM_PATHNAME)).to be false
+      expect(::File.fnmatch?('a?a', 'aba', ::File::FNM_PATHNAME)).to be true
+      expect(::File.fnmatch?('a?a', 'abba', ::File::FNM_PATHNAME)).to be false
+    end
+  end
+
+  it "can escape only the first [" do
+    expect(::File.fnmatch?('a\[b]a', 'a[b]a')).to be true
+    expect(::File.fnmatch?('a\[b]a', 'aba')).to be false
+  end
+
+  it "can escape the both []" do
+    expect(::File.fnmatch?('a\[b\]a', 'a[b]a')).to be true
+    expect(::File.fnmatch?('a\[b\]a', 'aba')).to be false
+  end
+end
